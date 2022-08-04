cd3f3820d4bd6078a8a2f8399b94625e91cff2e2
Uday Kiran <udaykiranreddy8050@gmail.com>
2022-08-04 10:32:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { useEffect } from "react";
 
+const url = "https://www.omdbapi.com/?apikey=bec039f0&s=batman";
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [loading,setLoading]=useState(true);
+ 
+  const etch = async () => {
+    try {
+
+      const response = await fetch(url);
+      const data = await response.json();
+      setMovies(data.Search);
+      setLoading(false);
+      console.log(data.Search);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const MoviesDisplay = () => {etch()};
+  // useEffect(() => {
+  //   etch();
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <div className="container">
+      <button className="btn" onClick={() => MoviesDisplay()}>
+        Fetch Movies
+      </button>
+      {!loading && movies.map((item) => {
+        return <>
+       <div className="container">
+        <img src={item.Poster} className="img" alt="" />
+        <p className="grid">{item.Title} ||<span>Year :: {item.Year}</span> </p>
+       </div>
+     
+        </>;
+      })}
+     
     </div>
+    </>
   );
 }
 
